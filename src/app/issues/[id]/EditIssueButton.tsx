365e4f90590a35623b3dd6b06f761933e0760bef
Prince Singh <prince.dev205@gmail.com>
2023-10-18 14:57:30 +0530

    Applying: SRP

---
+++ src/app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,16 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Button>
+      <Link href={`issues/${issueId}/edit`} className="flex gap-3 items-center">
+        <Pencil2Icon />
+        Edit Issue
+      </Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
 import { notFound } from "next/navigation";
 import prisma from "../../../../prisma/client";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import IssueStatusBadgae from "@/components/IssueStatusBadgae";
-import ReactMarkdown from "react-markdown";
-import Link from "next/link";
+import { Box,  Grid } from "@radix-ui/themes";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -19,22 +17,10 @@ const IssueDetailsPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex className="space-x-3" my="2">
-          <IssueStatusBadgae status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className="prose" mt="4">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Link href={`issues/${issue.id}/edit`} className="flex gap-3 items-center">
-            <Pencil2Icon />
-            Edit Issue
-          </Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
