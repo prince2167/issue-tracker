f3be01fe7467ee011b24953864ff71c3eda683d3
Prince Singh <prince.dev205@gmail.com>
2023-10-18 16:02:39 +0530

    build APIs for update issues

---
+++ src/app/api/issues/[id]/route.tsx
@@ -0,0 +1,33 @@
+import { IssueSchema } from "@/validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = IssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const issue = prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: parseInt(params.id) },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
