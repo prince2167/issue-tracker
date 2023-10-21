b4fdf816ade38681b801901f93b763bbd4b63d12
Prince Singh <prince.dev205@gmail.com>
2023-10-21 14:20:51 +0530

    create middleware

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
 import { IssueSchema } from "../../../validationSchemas";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextResponse) {
+    const session = await getServerSession(authOptions)
+    if (!session) return NextResponse.json({}, { status: 401 })
     const body = await request.json()
     const validation = IssueSchema.safeParse(body)
     if (!validation.success) {
--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -4,12 +4,15 @@ import { Box, Flex, Grid } from "@radix-ui/themes";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailsPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -20,12 +23,14 @@ const IssueDetailsPage = async ({ params }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="3">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="3">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
