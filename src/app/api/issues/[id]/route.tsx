c9ae001d6a6910dc4d6e5dfe8b70e2b1d40dfc3f
Prince Singh <prince.dev205@gmail.com>
2023-10-22 14:59:09 +0530

    enhance the api to assign issue

--- src/app/api/issues/[id]/route.tsx
+++ src/app/api/issues/[id]/route.tsx
@@ -1,4 +1,4 @@
-import { IssueSchema } from "@/validationSchemas";
+import {patchIssueSchema } from "@/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../../prisma/client";
 import { getServerSession } from "next-auth";
@@ -12,10 +12,22 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
+  
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { assignedToUserId, title, description } = body;
+  console.log(description,"ass");
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+  
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
   }
 
   const issue = await prisma.issue.findUnique({
@@ -29,8 +41,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId
     },
   });
 
@@ -43,7 +56,7 @@ export async function DELETE(
 ) {
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.json({}, { status: 401 });
-  
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
