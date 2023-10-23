b05dc5babb709ff231ee8d003b8c6e664ed42bf6
Prince Singh <prince.dev205@gmail.com>
2023-10-23 21:17:51 +0530

    build issue chart component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Pagination } from "@/components";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
 import prisma from "../../prisma/client";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -11,7 +12,7 @@ export default async function Home() {
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
     <main>
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <IssueChart open={open} inProgress={inProgress} closed={closed} />
     </main>
   );
 }
