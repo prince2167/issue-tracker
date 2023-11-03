987b2445f52e140bde2a4bab908ffcc38d054368
Prince Singh <prince.dev205@gmail.com>
2023-11-03 14:31:43 +0530

    refactoe latest issue component

--- src/app/LatestIssues.tsx
+++ src/app/LatestIssues.tsx
@@ -7,26 +7,34 @@ const LatestIssues = async () => {
   const issues = await prisma.issue.findMany({
     orderBy: { createdAt: "desc" },
     take: 5,
+    include: {
+      assignedToUser: true,
+    },
   });
   return (
     <Card>
-        <Heading size="4" mb="5">Latest Issues</Heading>
+      <Heading size="4" mb="5">
+        Latest Issues
+      </Heading>
       <Table.Root>
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Flex direction="column" align="start" gap="2">
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                  <IssueStatusBadgae status={issue.status} />
+                <Flex justify="between">
+                  <Flex direction="column" align="start" gap="2">
+                    <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                    <IssueStatusBadgae status={issue.status} />
+                  </Flex>
+                  {issue.assignedToUser && (
+                    <Avatar
+                      src={issue.assignedToUser.image!}
+                      fallback="?"
+                      size="2"
+                      radius="full"
+                    />
+                  )}
                 </Flex>
-                {/* {issue.assignedToUserId && (
-                <Avatar
-                  src={issue.assignedToUser.image!}
-                  fallback="?"
-                  radius="full"
-                />
-              )} */}
               </Table.Cell>
             </Table.Row>
           ))}
